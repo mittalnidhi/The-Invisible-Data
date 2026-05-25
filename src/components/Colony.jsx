5d3c7bb8c912fa2a9965ea6ad1d25ff4e7080b64
Nidhi Mittal <mittalnidhi.91@gmail.com>
2026-05-24 20:39:17 -0700

    reduced left & right padding of control panels in symptom atlas

--- src/components/Colony.jsx
+++ src/components/Colony.jsx
@@ -65,7 +65,7 @@ export default function Colony(props){
         <div className='relative colony-main flex flex-col h-[100vh] w-full z-30'>
             <div id="timeout-warning">Are you still there? Hover over the screen to continue, or return home in 10 seconds.</div>
             <ColonyNavBar />
-            <div className='relative flex flex-row justify-between w-[90vw] h-full min-h-150 mt-6 2xl:mt-12 mx-auto text-white'>
+            <div className='relative flex flex-row justify-between w-[94vw] h-full min-h-150 mt-6 2xl:mt-12 mx-auto text-white'>
                 <div className='relative flex flex-col h-[100%] w-[34%] min-w-[300px]'>
                     <div className='gray-panel-content flex flex-col h-full gap-4 z-20'>
                         <div className='flex flex-row gap-4 items-baseline'>
